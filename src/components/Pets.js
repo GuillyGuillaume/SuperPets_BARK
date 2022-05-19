9d842dbbad827468e898715419ecb1190c8c4ff8
William Zhang <wzhang26@uw.edu>
2022-05-19 00:40:01 -0700

    pet animate on adopt

--- src/components/Pets.js
+++ src/components/Pets.js
@@ -10,6 +10,9 @@ export function PetScreen() {
     const auth = getAuth();
     const navigate = useNavigate();
     const todayDate = new Date();
+    const [currDog, setDog] = React.useState("dog.png");
+    const [currCat, setCat] = React.useState("cat.png");
+    const [currHam, setHam] = React.useState("ham.png");
     const [dayCycle, setDayCycle] = React.useState(true);
     useEffect(() => {
         if(todayDate.getHours() > 20 || todayDate.getHours() < 5){
@@ -22,19 +25,28 @@ export function PetScreen() {
         const db = getDatabase();
         const petRef = ref(db, "users/"+auth.currentUser.uid+"/pet")
         fbset(petRef, "dog.png");
-        navigate('/');
+        setDog("talk_dog.png");
+        setTimeout(function(){
+            navigate('/');
+        }, 500);
     };
     const catClick = async () => {
         const db = getDatabase();
         const petRef = ref(db, "users/"+auth.currentUser.uid+"/pet")
         fbset(petRef, "cat.png");
-        navigate('/');
+        setCat("talk_cat.png");
+        setTimeout(function(){
+            navigate('/');
+        }, 500);
     };
     const hamClick = async () => {
         const db = getDatabase();
         const petRef = ref(db, "users/"+auth.currentUser.uid+"/pet")
         fbset(petRef, "ham.png");
-        navigate('/');
+        setHam("talk_ham.png");
+        setTimeout(function(){
+            navigate('/');
+        }, 500);
     };
     
 return (
@@ -42,19 +54,19 @@ return (
         <h1 className="page-title">Virtual Companions</h1>
         <div className="pets">
             <div className="pet-card">
-                <img src={'img/dog.png'}/>
+                <img src={'img/' + currDog}/>
                 <p></p>
                 <p>Name: BUDDY</p>
                 <button className="btn btn-warning" onClick={dogClick}>Adopt!</button>
             </div>
             <div className="pet-card">
-            <img src={'img/cat.png'}/>
+            <img src={'img/' + currCat}/>
                 <p></p>
                 <p>Name: LUNA</p>
                 <button className="btn btn-warning" onClick={catClick}>Adopt!</button>
             </div>
             <div className="pet-card">
-            <img src={'img/ham.png'}/>
+            <img src={'img/' + currHam}/>
                 <p></p>
                 <p>Name: CHEESE CURD</p>
                 <button className="btn btn-warning" onClick={hamClick}>Adopt!</button>
